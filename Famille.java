2b508a555c31ff90995cbc2848a1143dcdaf59dd
Marion Gain <marion.gain@outlook.fr>
2019-03-06 18:56:43 +0100

    Ajouté fichier Famille

---
+++ Famille.java
@@ -0,0 +1,47 @@
+class Famille {
+
+	// attributs
+	private boolean etat;
+	private Position origine;
+	private Position positionActuelle;
+
+	// constructeur par defaut
+	public Famille(){
+		this.etat = false;
+		this.origine = null;
+		this.positionActuelle = null;
+	}
+
+	// constructeur standard
+	public Famille(boolean e, Position o, Position a){
+		this.etat = e;
+		this.origine = o;
+		this.positionActuelle = a;
+	}
+
+	// accesseurs en lecture et ecriture
+	private boolean getEtat() {
+		return this.etat;
+	}
+
+	private Position getOrigine() {
+		return this.origine;
+	}
+
+	private Position getPosition() {
+		return this.positionActuelle;
+	}
+
+	private void setEtat(boolean e){
+		this.etat = e;
+	}
+
+	private void setOrigine(Position o){
+		this.origine = o;
+	}
+
+	private void setPosition(Position pa){
+		this.positionActuelle = pa;
+	}
+
+}
