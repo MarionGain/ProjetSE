b14fbe29658af60d2c78b3dca68618e540199231
Marion Gain <marion.gain@outlook.fr>
2019-03-06 18:58:53 +0100

    Ajouté fichier Robot

---
+++ Robot.java
@@ -0,0 +1,29 @@
+Class Robot extends Personnage {
+
+	// attributs 
+	private int niveau;
+	private Score score;
+
+	// constructeur par defaut
+
+	public Robot(){
+
+
+	}
+
+
+	// constructeur standard
+
+	public Robot (int niveau, Score score){
+
+	}
+	
+	
+
+	
+	private void saut(){
+
+	}
+
+
+}
