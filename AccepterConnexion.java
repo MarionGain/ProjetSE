40b432a49d17a7def8e9e783882cc446f146c6be
Marion Gain <marion.gain@outlook.fr>
2019-03-19 14:50:43 +0100

    Ajout fichier AccepterConnexion

---
+++ AccepterConnexion.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+class AccepterConnexion implements Runnable{
+	private ServerSocket ss;
+	private Socket socket = null;
+
+	public Thread t1; 
+
+	public AccepterConnexion(ServerSocket ss){
+		this.ss = ss;
+	}
+
+	public void run (){
+		
+		try{
+			while(true){
+				socket = ss.accept();
+				System.out.println("Un client se connecte");
+				// truc à rajouter
+				t1.start();
+			}
+		}
+		catch(IOException e){
+			System.err.println("Erreur serveur");
+		}
+	}
+}
