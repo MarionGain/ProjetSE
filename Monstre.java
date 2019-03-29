8619ae8e5ce01832c46fb5bc5082d1a57794c517
Dodovador <dorine.chassagne@sfr.fr>
2019-03-29 19:03:56 +0100

    ajout de la classe monstre avec sa position ainsi que osn image

--- Monstre.java
+++ Monstre.java
@@ -1,8 +1,52 @@
-class Monstre extends Personnage{
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import java.awt.Rectangle;
+
+class Monstre{
+    private Position posOrigine;
+    private Position posCourante;
+    private Image imMonstre;
+    private ImageIcon i;
+
+    private int x;
+    private int y;
+
+    public Monstre(){
+        loadImage();
+        posOrigine = new Position(50,50);
+        posCourante = new Position(50,50);
+    }
+
+    public void loadImage(){
+        i = new ImageIcon("monstre.png");
+        imMonstre = i.getImage(); 
+    }
+
+    public Image getImage(){
+        return this.imMonstre;
+    }
+
+    public int getHeight(){
+        return this.imMonstre.getHeight(null);
+    }
+
+    public int getWidth(){
+        return this.imMonstre.getWidth(null);
+    }
+
+    public Position getPosition(){
+        return posCourante;
+    }
+
+    public void setPosition(Position p){
+        this.posCourante=p;
+    }
 
 
-
-	public Monstre(){
-		super();
-	}
+    //Ajout
+    //retourne les "limitations" du rectangle de l image, necessaire pour la collision
+    public Rectangle getBounds() {
+        return new Rectangle(this.getPosition().getX(), this.getPosition().getY(), this.getWidth(), this.getHeight());
+    }
+    
 }
