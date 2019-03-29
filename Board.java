194cc1aba8eae3310585ad691ab7b17ec4388796
Dodovador <dorine.chassagne@sfr.fr>
2019-03-29 19:16:09 +0100

    Board: ajout de la map, attribut: ajout de variable provisoires pour afficher score, nbVies et numero du joueur, ajout d un tableau de fondDrol et d un choixFond qui donneras un chiffre aleatoire pour mettre une image de fond + constructeur: choix fond initialise  +  initFond(): permet de donner le chemin pour chaque image de fond et de les attribuer a chaque case du tableau  +  doFond: appelee dans paintComponant, permet de dessiner le fond  +  doMap(): creer la map avec tout les rectangles et affichage texte

--- Board.java
+++ Board.java
@@ -12,18 +12,39 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Rectangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 //ajout1 de la methode tirer appelant tir()
 
 
 class Board extends JPanel implements ActionListener{
+
     private Image i;
     private ImageIcon ii;
     private Timer timer;
+    private boolean ingame;
+    private int B_Width = 1000;
+    private int B_Height = 480;
 
     private Robot robot;
     private Projectile projectile;
     private Controller control;
+
+    //provisioire pour affichage
+    private int nbVies = 3;
+    private int score = 100000;
+    private int numJoueur = 1;
+
+    //image de fond
+    private FondDrol fond [] = new FondDrol[3];
+    private int choixFond;
+
 
     private int posInitX ;
     private int posInitY ;
@@ -31,38 +52,108 @@ class Board extends JPanel implements ActionListener{
     private int posCouranteX;
     private int posCouranteY;
 
-    private int vitesse = 3;
+    private int vitesse = 15;
+
+    
 
     //constructeur du Jpanel Board
     public Board(){
-        setBackground(Color.black);
 
         addKeyListener(new TAdapter());
         setFocusable(true);
+        initFond(fond);
+        setBackground(Color.black);
+        choixFond= (int)(Math.random()*3);
+
+        ingame = true;
+
+        setPreferredSize(new Dimension(B_Width, B_Height));
 
         robot = new Robot();
         projectile = new Projectile(robot);
-        control = new Controller(robot,projectile);
+        control = new Controller(this);
 
         timer = new Timer(vitesse, this);
         timer.start();
         
     }
 
+    //initialisation du tableau de fond du jeu
+    public void initFond(FondDrol [] f){
+        f[0] = new FondDrol("fond1.png");
+        f[1] = new FondDrol("fond2.png");
+        f[2] = new FondDrol("fond3.png");
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        doFond(g,fond,choixFond);
+        doMap(g);
         doDrawing(g);
         
         Toolkit.getDefaultToolkit().sync();
+    }
+
+    //affichage du fond
+    public void doFond(Graphics g, FondDrol [] f, int choix){
+
+        g.drawImage(f[choix].getImage(),0,0, this);
+
+    }
+
+    //affichage de la map
+    public void doMap(Graphics g){
+        Color c = g.getColor();
+        Color violet = new Color(85,37,112);
+        Color rose = new Color(252,35,255);
+        Font f = new Font(Font.SERIF, Font.BOLD, 80);
+        Font f2 = new Font(Font.SERIF, Font.BOLD, 20);
+
+        g.setColor(violet);
+        g.fillRect(0,(B_Height/4)-15,B_Width,10); // x,Y,largeur,hauteur
+        g.fillRect(0,0,1500,B_Height/4);
+
+        g.setColor(rose);
+        g.setFont(f);
+        g.drawString("DROL",(B_Width/2)-125,20+B_Height/8);
+
+        g.setFont(f2);
+        g.drawString("JOUEUR "+numJoueur,B_Width/8-120,B_Height/8-20);
+        g.drawString("SCORE      "+score,B_Width/8-120,B_Height/8+10);
+        g.drawString("NOMBRE DE VIES     "+nbVies,B_Width/8-120,B_Height/8+40);
+        g.drawString("HIGHSCORE",3*B_Width/4+40,B_Height/8-20);
+        
+
+        g.setColor(Color.BLUE);
+        g.fillRect(50+B_Width/4,0,10,B_Height/4);
+
+        g.fillRect(3*B_Width/4-50,0,10,B_Height/4);
+
+
+        g.setColor(Color.GRAY);
+        g.fillRect(0,B_Height-9*(B_Height/16)-10,B_Width,10);
+
+        g.setColor(Color.GRAY);
+        g.fillRect(0,B_Height-3*(B_Height/8)-10,B_Width,10);
+
+        g.setColor(Color.GRAY);
+        g.fillRect(0,B_Height-3*(B_Height/16)-10,B_Width,10);
+
+        g.setColor(Color.GRAY);
+        g.fillRect(0,B_Height-10,B_Width,10);
+ 
+        g.setColor(c);
     }
     
     private void doDrawing(Graphics g) {
         
         Graphics2D g2d = (Graphics2D) g;
-
-        g2d.drawImage(robot.getImage(), robot.getPosition().getX(), robot.getPosition().getY(), this);
-        //ajout2
+        
+        if(robot.isVisible()){
+            g2d.drawImage(robot.getImage(), robot.getPosition().getX(), robot.getPosition().getY(), this);
+        }
+        
         if(projectile.isVisible()==true){
             g2d.drawImage(projectile.getImage(), projectile.getPosition().getX(),projectile.getPosition().getY(), this);
         }
@@ -85,5 +176,21 @@ class Board extends JPanel implements ActionListener{
         public void keyPressed(KeyEvent e) {
             control.keyPressed(e);
         }
+    }
+
+    public int getB_Width(){
+        return this.B_Width;
+    }
+
+    public int getB_Height(){
+        return this.B_Height;
+    }
+
+    public Robot getRobot(){
+        return this.robot;
+    }
+
+    public Projectile getProjectile(){
+        return this.projectile;
     }
 }
