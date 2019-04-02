e4058d361ff2e96c8491f80b5442ba4f5e0c8821
Dodovador <dorine.chassagne@sfr.fr>
2019-04-02 23:23:10 +0200

    Etage: ajout d une liste de trappe et d une trappe, on cree un deuxieme constructeur qui prend en parametre une lsite de trappes et un int y, ajout de getListTrappes

--- Etage.java
+++ Etage.java
@@ -1,40 +1,47 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Etage {
 
 	// attributs 
 	private int id;
-	//private Famille famille;
 	private Position position;
+	private int width;
+	private int height;
+	private int B_width = 1000;
+	private Trappe trappe;
+	private List<Trappe> trappes;
 
 	// constructeur par defaut
 	public Etage(){
-		//this.id = ;
-		//this.famille = null;
+		this.trappes = null;
 		this.position = null;
-
+		this.width = B_width;
+        this.height = 10;
 	}
 
 	// constructeur standard 
 	//a rajoute id et famille en parametre
-	public Etage(int y){
+	public Etage(List<Trappe> trappes,int y){
 		//this.id = id;
-		//this.famille = f;
+		this.trappes = trappes;
 		this.position = new Position(0,y);
+		this.width = B_width;
+        this.height = 10;
+	}
+
+	public Etage(int y){
+		this.trappes = null;
+		this.position = new Position(0,y);
+		this.width = B_width;
+        this.height = 10;
 	}
 
 	// accesseurs en lecture et ecriture
 	public int getId (){
 		return this.id;
 	}
-
-	// private Famille getFamille(){
-	// 	return this.famille;
-	// }
-
-	// private void setFamille(Famille f){
-	// 	this.famille = f;
-	// }
 
 	public Position getPosition(){
 		return this.position;
@@ -44,8 +51,24 @@ class Etage {
 		this.position = p;
 	}
 
+	public List<Trappe> getListTrappes(){
+		return this.trappes;
+	}
+
 	public void doEtage(Graphics g, int x, int y, int largeur, int hauteur ){
 		g.fillRect(x,y,largeur,hauteur);
+	}
+
+	public int getWidth(){
+		return this.width;
+	}
+
+	public int getHeight(){
+		return this.height;
+	}
+
+	public Rectangle getBounds(){
+		return new Rectangle(this.getPosition().getX(), this.getPosition().getY(), this.getWidth(), this.getHeight());
 	}
 
 
