50af6b174fcca7ff30c25d4bffa32f0128a26724
Marion Gain <marion.gain@outlook.fr>
2019-04-03 19:29:43 +0200

    Modification Drol -> tests gestion du classement

--- Drol.java
+++ Drol.java
@@ -6,10 +6,21 @@ import java.awt.event.WindowEvent;
 public class Drol extends JFrame{
 
     private Board b;
+    private Classement classement;
 
     public Drol(){
         initUI();
+        this.classement = new Classement();
+
     }
+
+    public Board getBoard(){
+        return this.b;
+    }
+
+
+
+
 
     public void initUI(){
         this.b = new Board();
@@ -18,10 +29,12 @@ public class Drol extends JFrame{
         //this.setSize(1500,1000);
         pack();
 
+
         this.setTitle("DROL");
-        this.addWindowListener(new WindowAdapter()){
+        this.addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent e){
                     // ajouter saveScore
+                // Drol.getBoard().getRobot().getScore().saveScore(Drol.getBoard().getRobot().getNom());
             }
         });
 
@@ -32,9 +45,7 @@ public class Drol extends JFrame{
         this.setVisible(true);
     }
 
-    public Board getBoard(){
-        return this.b;
-    }
+   
 
     public static void main(String args[]){
         Drol drol = new Drol();
@@ -42,11 +53,18 @@ public class Drol extends JFrame{
         Partie partie = new Partie(drol.getBoard()); // ajouter board 
 
         // // partie.creationServeur();
-        partie.menu();
+        //partie.menu();
         System.out.println("bla");
-        // drol.getBoard().getRobot().setNom("Marion");
+        drol.getBoard().getRobot().setNom("Marion");
         drol.getBoard().getRobot().getScore().ajoutMonstre(10);
         drol.getBoard().getRobot().getScore().saveScore(drol.getBoard().getRobot().getNom());
+        drol.getBoard().getRobot().getScore().ajoutMonstre(100);
+        drol.getBoard().getRobot().getScore().saveScore(drol.getBoard().getRobot().getNom());
+        drol.getBoard().getRobot().getScore().ajoutMonstre(19);
+        drol.getBoard().getRobot().getScore().saveScore(drol.getBoard().getRobot().getNom());
+        drol.getBoard().getRobot().getScore().ajoutMonstre(3);
+        drol.getBoard().getRobot().getScore().saveScore(drol.getBoard().getRobot().getNom());
+         drol.classement.initClassement("score.txt");
         //drol.getBoard().getRobot().getScore().saveScore();
         // partie.creationClient();
         // partie.rejoindrePartie();
