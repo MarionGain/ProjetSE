f1b9d4fbb233bf821c11a8d1760f00a0e606f87c
Dodovador <dorine.chassagne@sfr.fr>
2019-04-09 11:56:34 +0200

    modification Donnee conversion StringDonnee return une donee et plus une arraylist + ajout de isRobot isTrappe etc

--- Donnees.java
+++ Donnees.java
@@ -3,58 +3,123 @@ import java.util.*;
 class Donnees {
 
 	private int emetteur;
-	private int type;
-	private int objet;
-	private int action;
-	private int deplacement;
+	private int type;	
+	private int objet;	//indice
+	private int arg1;	//position x       //pour score : score
+	private int arg2;	//position y
+	private int arg3;	//visible  //pour le clavier : si tir ou pastir
 	//private ArrayList<String> donnees;
 
-	public Donnees (int emetteur, int objet, int action, int deplacement) {
+
+	public Donnees (int emetteur,int type, int objet, int arg1, int arg2, int arg3) {
 
 		this.emetteur = emetteur;
 		this.type = type;
 		this.objet = objet;
-		this.action = action;
-		this.deplacement = deplacement;
+		this.arg1 = arg1;	
+		this.arg2 = arg2;	
+		this.arg3 = arg3;	
+
 	}
 
 	public Donnees(){
 		this.emetteur = -2;
 		this.type = -2;
 		this.objet = -2;
-		this.action = -2;
-		this.deplacement = -2;
+	}
+
+	public int getEmetteur(){
+		return this.emetteur;
+	}
+
+	public int getType(){
+		return this.type;
+	}
+
+	public int getObjet(){
+		return this.objet;
+	}
+
+	public int getArg1(){
+		return this.arg1;
+	}
+
+	public int getArg2(){
+		return this.arg2;
+	}
+
+	public int getArg3(){
+		return this.arg3;
+	}
+
+	public void setArg3(int visible){
+		this.arg3 = visible;
 	}
 
 	public String conversionDonneesString(){
 		
-		String s = "";
-		s += Consts.DEBUTCHAINE + this.emetteur + Consts.SEPARCHAINE + this.type + Consts.SEPARCHAINE + this.objet + Consts.SEPARCHAINE + this.action + Consts.SEPARCHAINE + this.deplacement;
+		String s = Consts.DEBUTCHAINE + String.valueOf(this.emetteur) + Consts.SEPARCHAINE + String.valueOf(this.type) + Consts.SEPARCHAINE + String.valueOf(this.objet) + Consts.SEPARCHAINE + String.valueOf(this.arg1) + Consts.SEPARCHAINE + String.valueOf(this.arg2) + Consts.SEPARCHAINE + String.valueOf(this.arg3) + Consts.FINCHAINE ;
 		return s;
 	}
 
-	public ArrayList<Donnees> conversionStringDonnees(String s){
-		
-		ArrayList<Donnees> donnees = new ArrayList<Donnees>();
-		if (s.equals("") == false){
-			String[] tabString = s.split(Consts.DEBUTCHAINE);
-			for (String a : tabString){
-				if(a.equals("") == false){
-					String [] d = a.split(Consts.SEPARCHAINE);
-					Donnees data = null;
-					if(d.length == 3){
-						data = new Donnees(Integer.parseInt(d[0]), Integer.parseInt(d[1]), Integer.parseInt(d[2]), -1);
-					}
-				
-					else if(d.length == 4){
-						data = new Donnees(Integer.parseInt(d[0]), Integer.parseInt(d[1]), Integer.parseInt(d[2]), Integer.parseInt(d[3]));
-					}
-					donnees.add(data);				
-				}
-			}
+        public Donnees conversionStringDonnees(String s){
+			//System.out.println("chaine conversion stringdonnees "+ s);
+
+            Donnees data = new Donnees();
+            s = s.replaceAll(Consts.DEBUTCHAINE , "");
+            s = s.replaceAll(Consts.FINCHAINE , "");
+            if(s.equals("") == false){
+                String [] d = s.split(Consts.SEPARCHAINE);
+                data = new Donnees(Integer.parseInt(d[0]), Integer.parseInt(d[1]), Integer.parseInt(d[2]), Integer.parseInt(d[3]), Integer.parseInt(d[4]), Integer.parseInt(d[5]));
+            }
+
+            return data;
+	}
+
+        public boolean isRobot(){
+			if(this.type == Consts.ROBOT ) return true;
+			else return false;
 		}
 
-		return donnees;
-	}
+		
+		public boolean isProjectile(){
+			if(this.type == Consts.PROJECTILE) return true;
+			else return false;
+		}
+		
+		public boolean isMonstre(){
+			if(this.type == Consts.MONSTRE) return true;
+			else return false;
+		}
+		
+		public boolean isFamille(){
+			if(this.type == Consts.FAMILLE) return true;
+			else return false;
+		}
+		
+		public boolean isFond(){
+			if(this.type == Consts.FOND) return true;
+			else return false;
+		}
+		
+		public boolean isTrappe(){
+			if(this.type == Consts.TRAPPE) return true;
+			else return false;
+		}
+
+		public boolean isClavier(){
+			if(this.type == Consts.CLAVIER) return true;
+			else return false;
+		}
+
+		public boolean isScore(){
+			if(this.type == Consts.SCORE) return true;
+			else return false;
+		}
+
+		public boolean isNbVies(){
+			if(this.type == Consts.NBVIES) return true;
+			else return false;
+		}
 
 }
