603223e88c986942291e8453a21938361f238c00
Dodovador <dorine.chassagne@sfr.fr>
2019-04-09 11:58:09 +0200

    ajout classe emission permet d envoyer donnee client serveur et serveur client

---
+++ Emission.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.net.*;
+
+import java.util.Scanner;
+
+class Emission /*implements Runnable*/{
+
+    private PrintWriter out;
+    private Controller controller;
+    
+    public Emission(Controller c,Socket s){
+
+        try{
+            this.out = new PrintWriter(s.getOutputStream());
+            this.controller = c;
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+       
+	}
+
+    public void push(Donnees data){
+        String message = data.conversionDonneesString();
+        out.println(message);
+        out.flush();
+    }
+
+	// public void run(){
+
+		
+	// 	while(true){ 
+    //         try{
+    //             this.sleep(1);
+    //         }catch(IOException e){
+    //             e.printStackTrace();
+    //         }
+
+    //     }
+
+			
+	// }
+}
